93e7cebc3f131de8d407489cea2f59d642464ffb
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2024-03-14 14:09:43 +0530

    Add files via upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL="img/"
